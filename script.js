071924d227409b6206496ee8016560dabf21e0d6
JayFarei <gabriele.farei@r3.com>
2023-12-13 17:13:58 +0000

    feat: first iteration

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed(".typed-text", {
+    strings: ["Hashlock Dynamics"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    backDelay: 2000,
+  });
+});
